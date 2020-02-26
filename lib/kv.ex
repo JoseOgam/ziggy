20daf4d0e1982795aee9b905137a588198f328a9
JoseOgam <jotienoochieng@gmail.com>
2020-02-26 08:37:51 +0300

    first commit

---
+++ lib/kv.ex
@@ -0,0 +1,19 @@
+defmodule KV do
+
+ @moduledoc """
+  Documentation for KV.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KV.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
